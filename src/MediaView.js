bf78f6ef20d1a29624f9605cb8f747c32bd779a1
atoool <scrapycoco.sam@gmail.com>
2021-04-27 12:06:10 +0530

    first commit

---
+++ src/MediaView.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Dimensions} from 'react-native';
+import {NativeMediaView} from 'react-native-admob-native-ads';
+import {Logger} from './utils';
+
+export const MediaView = ({aspectRatio = 1.5}) => {
+  const onVideoPlay = () => {
+    Logger('VIDEO', 'PLAY', 'Video is now playing');
+  };
+
+  const onVideoPause = () => {
+    Logger('VIDEO', 'PAUSED', 'Video is now paused');
+  };
+
+  const onVideoProgress = event => {
+    Logger('VIDEO', 'PROGRESS UPDATE', event);
+  };
+
+  const onVideoEnd = () => {
+    Logger('VIDEO', 'ENDED', 'Video end reached');
+  };
+
+  const onVideoMute = muted => {
+    Logger('VIDEO', 'MUTE', muted);
+  };
+
+  return (
+    <NativeMediaView
+      style={{
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+        backgroundColor: '#000',
+      }}
+      onVideoPause={onVideoPause}
+      onVideoPlay={onVideoPlay}
+      onVideoEnd={onVideoEnd}
+      onVideoProgress={onVideoProgress}
+      onVideoMute={onVideoMute}
+    />
+  );
+};
